9dfe0e9dcad3fc9a5e0feb36af000ee877cd91c6
devpcjin <sgn03077@gmail.com>
2022-04-21 01:12:32 +0900

    complete function in news.py index.html

--- crawling/news.py
+++ crawling/news.py
@@ -1,44 +1,60 @@
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
 import requests
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.dbsparject
+
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://finance.naver.com/news/mainnews.naver',headers=headers)
+@app.route('/memo', methods=['GET'])
+def listing():
+    articles = list(db.news.find({}, {'_id': False}))
+    return jsonify({'all_articles':articles})
 
-soup = BeautifulSoup(data.text, 'html.parser')
+@app.route('/news', methods=['POST'])
+def show_news():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('https://finance.naver.com/news/mainnews.naver', headers=headers)
 
-#contentarea_left > div.mainNewsList > ul > li:nth-child(1)
-#contentarea_left > div.mainNewsList > ul > li:nth-child(2)
+    soup = BeautifulSoup(data.text, 'html.parser')
 
-articles = soup.select("#contentarea_left > div.mainNewsList > ul > li")
+    articles = soup.select("#contentarea_left > div.mainNewsList > ul > li")
 
-#contentarea_left > div.mainNewsList > ul > li:nth-child(1) > dl > dt > a
-#contentarea_left > div.mainNewsList > ul > li:nth-child(2) > dl > dd.articleSubject > a
-#contentarea_left > div.mainNewsList > ul > li:nth-child(2) > dl > dd.articleSummary
-#contentarea_left > div.mainNewsList > ul > li:nth-child(2) > dl > dt > a > img
+    doc = []
 
-for span_tag in soup.findAll('span'):
-    span_tag.replace_with('')
+    db.news.delete_many({})
 
-for ar in articles:
-    a_tag = ar.select_one('dl > dd.articleSubject > a')
-    if a_tag is not None:
-        title = ar.select_one('dl > dd.articleSubject > a').text
-        summury = ar.select_one('dl > dd.articleSummary').text.strip()
-        img = ar.select_one('dl > dt > a > img')
-        url = ar.select_one('dl > dd.articleSubject > a')
+    for span_tag in soup.findAll('span'):
+        span_tag.replace_with('')
 
-        print(title)
-        print(summury)
-        print(img['src'])
-        print('https://finance.naver.com/'+url['href'])
+    for ar in articles:
+        a_tag = ar.select_one('dl > dd.articleSubject > a')
+        if a_tag is not None:
+            title = ar.select_one('dl > dd.articleSubject > a').text
+            summury = ar.select_one('dl > dd.articleSummary').text.strip()
+            img = ar.select_one('dl > dt > a > img')['src']
+            url = 'https://finance.naver.com/' + ar.select_one('dl > dd.articleSubject > a')['href']
+
+            doc = {'title': title, 'summury': summury, 'img': img, 'url': url}
+
+            db.news.insert_one(doc)
+
+    return jsonify({'msg':'뉴스 최신화'})
+
+
+
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
+
+
