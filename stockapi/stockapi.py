68c1583aff507a4ae15b0c8fab5226a6e58e37fd
devpcjin <sgn03077@gmail.com>
2022-04-21 17:15:46 +0900

    Update typo in stockapi.py

--- stockapi/stockapi.py
+++ stockapi/stockapi.py
@@ -2,7 +2,7 @@ import requests
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.dbsparject
 
 r = requests.get('https://m.stock.naver.com/api/json/sise/enrollItemListJson.nhn?pageSize=100')
 
@@ -11,7 +11,7 @@ stockapi = r.json().get('result').get('itemList')
 for i in range(len(stockapi)):
     stock_date = stockapi[i]['thistime'][0:8]
     stock_name = stockapi[i]['nm']
-    stock_price = stockapi[i]['cv']
+    stock_price = stockapi[i]['pcv']
     stock_rate = stockapi[i]['cr']
 
     print(stock_date, stock_name, stock_price, stock_rate)
@@ -24,5 +24,3 @@ for i in range(len(stockapi)):
 
     }
     db.stockapi.insert_one(doc)
-
-    print(doc)
