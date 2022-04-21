e26888f7ed815529809ee886ee86cf1b155eae95
devpcjin <sgn03077@gmail.com>
2022-04-21 18:14:37 +0900

    Update reload DB and price-change in stockapi.py

--- stockapi/stockapi.py
+++ stockapi/stockapi.py
@@ -8,10 +8,13 @@ r = requests.get('https://m.stock.naver.com/api/json/sise/enrollItemListJson.nhn
 
 stockapi = r.json().get('result').get('itemList')
 
+db.stockapi.delete_many({})
+
 for i in range(len(stockapi)):
     stock_date = stockapi[i]['thistime'][0:8]
     stock_name = stockapi[i]['nm']
     stock_price = stockapi[i]['pcv']
+    stock_pchange = stockapi[i]['cv']
     stock_rate = stockapi[i]['cr']
 
     print(stock_date, stock_name, stock_price, stock_rate)
@@ -20,7 +23,7 @@ for i in range(len(stockapi)):
         'Date': stock_date,
         'Name': stock_name,
         'Price': stock_price,
-        'Range': stock_rate
-
+        'Pchange': stock_pchange,
+        'Range': stock_rate,
     }
     db.stockapi.insert_one(doc)
